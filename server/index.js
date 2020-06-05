0683440e163915e3d853c0dcb6373039fcdf6e84
gen0pp <gen0@protonmail.com>
2020-06-05 16:16:30 -0700

    loader.io adjustments

--- server/index.js
+++ server/index.js
@@ -8,33 +8,46 @@ const port = 2112;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-const mediaProxy = createProxyMiddleware({
-  target: 'http://localhost:8000',
-  changeOrigin: true,
-});
+app.use('/media', createProxyMiddleware({ target: 'http://13.57.57.14:8000', changeOrigin: true}));
+
+// const mediaProxy = createProxyMiddleware({
+//   target: 'http://13.57.57.14:8000/',
+//   changeOrigin: true,
+// });
 
 // const reviewProxy = createProxyMiddleware({
-//   target: 'http://54.67.101.150:4200',
+//   target: 'http://8.224.107.83:4200/', 
 //   changeOrigin: true,
 // });
 
 // const announcementProxy = createProxyMiddleware({
-//   target: 'http://3.17.138.9:8080',
+//   target: '',
 //   changeOrigin: true,
 // });
 
 // const bodyProxy = createProxyMiddleware({
-//   target: 'http://52.14.114.30:1992',
+//   target: 'http://ec2-3-23-61-151.us-east-2.compute.amazonaws.com:1992/',
 //   changeOrigin: true,
 // });
 
-app.use('/media', mediaProxy);
+//app.use('/media', mediaProxy);
 
 // app.use('/api/reviews/:id', reviewProxy);
 
 // app.use('/getGame', announcementProxy);
 
 // app.use('/mainbody', bodyProxy);
+
+app.get('/loaderio-4b3718784fc1542b18ab3d2134f121fe/', (req, res) => {
+  const callback = (err, data) => {
+    if (err) {
+      res.status(404).json(err);
+    } else {
+      res.status(200).json(data);
+    }
+  };  
+  res.sendFile('/loaderio-4b3718784fc1542b18ab3d2134f121fe.txt', callback);
+});
 
 
 app.use(parser.json());
