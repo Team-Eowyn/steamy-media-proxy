125607b34ed0cd57c8f588c82b89a92ec00b805b
gen0pp <gen0@protonmail.com>
2020-06-05 15:47:56 -0700

    Proxy adjustments

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const express = require('express');
 const app = express();
 const { createProxyMiddleware } = require('http-proxy-middleware');
@@ -8,32 +9,32 @@ const port = 2112;
 app.use(express.static(path.join(__dirname, '../public')));
 
 const mediaProxy = createProxyMiddleware({
-  target: 'http://3.23.132.230:8000',
+  target: 'http://localhost:8000',
   changeOrigin: true,
 });
 
-const reviewProxy = createProxyMiddleware({
-  target: 'http://54.67.101.150:4200',
-  changeOrigin: true,
-});
+// const reviewProxy = createProxyMiddleware({
+//   target: 'http://54.67.101.150:4200',
+//   changeOrigin: true,
+// });
 
-const announcementProxy = createProxyMiddleware({
-  target: 'http://3.17.138.9:8080',
-  changeOrigin: true,
-});
+// const announcementProxy = createProxyMiddleware({
+//   target: 'http://3.17.138.9:8080',
+//   changeOrigin: true,
+// });
 
-const bodyProxy = createProxyMiddleware({
-  target: 'http://52.14.114.30:1992',
-  changeOrigin: true,
-});
+// const bodyProxy = createProxyMiddleware({
+//   target: 'http://52.14.114.30:1992',
+//   changeOrigin: true,
+// });
 
 app.use('/media', mediaProxy);
 
-app.use('/api/reviews/:id', reviewProxy);
+// app.use('/api/reviews/:id', reviewProxy);
 
-app.use('/getGame', announcementProxy);
+// app.use('/getGame', announcementProxy);
 
-app.use('/mainbody', bodyProxy);
+// app.use('/mainbody', bodyProxy);
 
 
 app.use(parser.json());
